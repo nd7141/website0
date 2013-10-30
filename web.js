91d4391fe76a0cb87e5a6101e9345013f904cb8e
Ivanov Sergey <ivanovserg990@gmail.com>
2013-10-30 11:57:20 +0400

    Use bootstrap template inside the app

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello, there. Ready to deploy new web app!');
+  //response.send('Hello, there.');
+    var htmlFile = fs.readFileSync('./pages/new-template/stylish-portfolio.html');
+    response.send(htmlFile.toString());   
 });
 
 var port = process.env.PORT || 5000;
